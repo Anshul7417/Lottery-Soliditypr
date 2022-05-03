8aa35a448befc221be3defe43cc58db9505d6849
Anshul Tripathi <tripathianshul06@gmail.com>
2022-05-03 13:59:41 +0530

    winner functionality added

--- lottery.sol
+++ lottery.sol
@@ -1,4 +1,4 @@
-// SPDX-License_Identifier: GPL-3.0
+ // SPDX-License_Identifier: GPL-3.0
 pragma solidity >=0.5.0 <0.9.0;
 
 
@@ -10,11 +10,30 @@ contract lottery{
         manager=msg.sender;
     }
 
-    receive() external payable{  //special one time used to transfer ether to contract
+    receive() external payable{  //special function executes one time used to transfer ether to contract
+        require(msg.value == 2 ether);    //if else short form
         participants.push(payable(msg.sender));
     }
 
     function getBalance() public view returns(uint){
+        require(msg.sender == manager);    // visible to manager only
         return address(this).balance;
     }
+
+
+    function random() public view returns(uint){
+       return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));   // generates random values
+    }
+
+    function selectWinner() public payable {
+        require(msg.sender == manager);
+        require(participants.length>=3);
+        uint r = random();
+        address payable winner;
+        uint index = r % participants.length;
+        winner = participants[index];
+        winner.transfer(getBalance());
+        participants = new address payable[](0);   // resetting after winner announcement.
+    }
+
 }
